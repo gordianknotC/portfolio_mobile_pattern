da297844e8bfe77dc45c74efc071682e1c44ef2f
curtis <curtis@fillygaming888.com>
2023-02-10 17:21:12 +0800

    initial commit

---
+++ precache-manifest.9468e9ee2b6e71af3175e85d0e2211b0.js
@@ -0,0 +1,382 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe1fffe32a4fadb7163",
+    "url": "/portfolio_mobile_pattern/css/app.891e7bf4.css"
+  },
+  {
+    "revision": "51a7abebf2892feb30ec",
+    "url": "/portfolio_mobile_pattern/css/chunk-07bfe84c.55e982d3.css"
+  },
+  {
+    "revision": "f919fe2547b049d0a797",
+    "url": "/portfolio_mobile_pattern/css/chunk-0be5c82e.7f7f8ed6.css"
+  },
+  {
+    "revision": "5d690eedcb4830882599",
+    "url": "/portfolio_mobile_pattern/css/chunk-0c247ecf.6a54a6e6.css"
+  },
+  {
+    "revision": "71592820b39261a3b776",
+    "url": "/portfolio_mobile_pattern/css/chunk-0cb0f95c.d3b70842.css"
+  },
+  {
+    "revision": "b03a9f55d74403074ab8",
+    "url": "/portfolio_mobile_pattern/css/chunk-0f7b1216.9c4cc7d1.css"
+  },
+  {
+    "revision": "0ade9266c6802b3b2389",
+    "url": "/portfolio_mobile_pattern/css/chunk-11936de2.1ec38fbe.css"
+  },
+  {
+    "revision": "7bf7c0297b2008e0eb77",
+    "url": "/portfolio_mobile_pattern/css/chunk-19f763a4.72c5c2b1.css"
+  },
+  {
+    "revision": "6d7a50586937d30b8284",
+    "url": "/portfolio_mobile_pattern/css/chunk-1b61b144.2b6f389f.css"
+  },
+  {
+    "revision": "57c13f6a05600c0704f3",
+    "url": "/portfolio_mobile_pattern/css/chunk-1d4eb874.17760b45.css"
+  },
+  {
+    "revision": "fccff58bb1dd9fc356f7",
+    "url": "/portfolio_mobile_pattern/css/chunk-1d6b9686.5a314559.css"
+  },
+  {
+    "revision": "b50b4c08822e0ecda3ea",
+    "url": "/portfolio_mobile_pattern/css/chunk-2090f038.a0c09c45.css"
+  },
+  {
+    "revision": "1b1faaaebeecdcf2f045",
+    "url": "/portfolio_mobile_pattern/css/chunk-227cb02f.a845d4a6.css"
+  },
+  {
+    "revision": "4c9371ba5c95fd1275ba",
+    "url": "/portfolio_mobile_pattern/css/chunk-2b09266e.37aee9ab.css"
+  },
+  {
+    "revision": "669fcdfeb54794269fc8",
+    "url": "/portfolio_mobile_pattern/css/chunk-2c5b5e18.fd579fe5.css"
+  },
+  {
+    "revision": "0330c7214e4fa6b255fc",
+    "url": "/portfolio_mobile_pattern/css/chunk-2e5f61a5.09fbf8fe.css"
+  },
+  {
+    "revision": "14802a9d8e6eab944dac",
+    "url": "/portfolio_mobile_pattern/css/chunk-2e978e37.9630de3a.css"
+  },
+  {
+    "revision": "3c25afd72752d717a481",
+    "url": "/portfolio_mobile_pattern/css/chunk-38b64240.ca1e4c66.css"
+  },
+  {
+    "revision": "a32b3feccc4b6d6e0826",
+    "url": "/portfolio_mobile_pattern/css/chunk-46fa2373.7b2b3d5e.css"
+  },
+  {
+    "revision": "617833d6b4c50ce7ac55",
+    "url": "/portfolio_mobile_pattern/css/chunk-4790fb3f.2d6d1256.css"
+  },
+  {
+    "revision": "d48826e0f96d71a8d6e9",
+    "url": "/portfolio_mobile_pattern/css/chunk-483e105e.61d0f3eb.css"
+  },
+  {
+    "revision": "8991c894f5383676954b",
+    "url": "/portfolio_mobile_pattern/css/chunk-5510352e.4e2bd227.css"
+  },
+  {
+    "revision": "93543f6750a8f9650444",
+    "url": "/portfolio_mobile_pattern/css/chunk-5c758f93.5dec985f.css"
+  },
+  {
+    "revision": "e7170a3345e74d0aa7f9",
+    "url": "/portfolio_mobile_pattern/css/chunk-605577a4.34c9727f.css"
+  },
+  {
+    "revision": "cbc3969e27727e1081b8",
+    "url": "/portfolio_mobile_pattern/css/chunk-6385379c.2c24c1ad.css"
+  },
+  {
+    "revision": "4b921fe7ff55f68f6f49",
+    "url": "/portfolio_mobile_pattern/css/chunk-695745a4.c675a270.css"
+  },
+  {
+    "revision": "02e41295d7c063a89483",
+    "url": "/portfolio_mobile_pattern/css/chunk-6b57e0f0.130953d6.css"
+  },
+  {
+    "revision": "dfc0b89dd7f2285b30fb",
+    "url": "/portfolio_mobile_pattern/css/chunk-6bf98b4e.c3c9dabd.css"
+  },
+  {
+    "revision": "f93ec1d1e9ad764f9b7d",
+    "url": "/portfolio_mobile_pattern/css/chunk-70cf529a.d55d45eb.css"
+  },
+  {
+    "revision": "68068f071d41f52ce556",
+    "url": "/portfolio_mobile_pattern/css/chunk-77d94120.b4105e44.css"
+  },
+  {
+    "revision": "954d61a252e3b54539c0",
+    "url": "/portfolio_mobile_pattern/css/chunk-8337512c.f081acd0.css"
+  },
+  {
+    "revision": "c5822240907b077056fb",
+    "url": "/portfolio_mobile_pattern/css/chunk-87753a5a.8ccd6f89.css"
+  },
+  {
+    "revision": "120beb6c1fc2181fb4b9",
+    "url": "/portfolio_mobile_pattern/css/chunk-8ff88cf6.09610783.css"
+  },
+  {
+    "revision": "3bcc4310684bbba2af1a",
+    "url": "/portfolio_mobile_pattern/css/chunk-9cd9bf06.ae24a6a6.css"
+  },
+  {
+    "revision": "50f4c84d30bae46c8530",
+    "url": "/portfolio_mobile_pattern/css/chunk-a309b180.16cb4de4.css"
+  },
+  {
+    "revision": "45d81d0c952fffb37fa9",
+    "url": "/portfolio_mobile_pattern/css/chunk-ce463998.d5b1f2c4.css"
+  },
+  {
+    "revision": "212f4c05f2bb4c985beb",
+    "url": "/portfolio_mobile_pattern/css/chunk-cf54d8f8.b15c6f56.css"
+  },
+  {
+    "revision": "723376a59093dee7aef5",
+    "url": "/portfolio_mobile_pattern/css/chunk-e2efb400.d50a1b0b.css"
+  },
+  {
+    "revision": "83c17b20db3fdfcf7a68",
+    "url": "/portfolio_mobile_pattern/css/chunk-f3f2185e.9b62815e.css"
+  },
+  {
+    "revision": "5fffa1e1da008954e105",
+    "url": "/portfolio_mobile_pattern/css/chunk-vendors.b0c0533d.css"
+  },
+  {
+    "revision": "2d1eb765335816d2ee2a128eec8978be",
+    "url": "/portfolio_mobile_pattern/fonts/Lexend-Medium.2d1eb765.ttf"
+  },
+  {
+    "revision": "fd2d8ea04e25585558f2396e4fa56c06",
+    "url": "/portfolio_mobile_pattern/fonts/Lexend-Regular.fd2d8ea0.ttf"
+  },
+  {
+    "revision": "cea9837cea4d732f8009ac063c247229",
+    "url": "/portfolio_mobile_pattern/img/avatar.cea9837c.png"
+  },
+  {
+    "revision": "319b3d3772f06ca78ca3d60480d8ecf5",
+    "url": "/portfolio_mobile_pattern/img/fandream11.319b3d37.png"
+  },
+  {
+    "revision": "7930f089ee664236e5fb9bb41893dcb5",
+    "url": "/portfolio_mobile_pattern/img/invite.7930f089.png"
+  },
+  {
+    "revision": "2defe90d5696d42b2fd0330df33b7292",
+    "url": "/portfolio_mobile_pattern/img/wheel-big.2defe90d.png"
+  },
+  {
+    "revision": "d48597eb41917bda7d75fe1044a2495e",
+    "url": "/portfolio_mobile_pattern/index.html"
+  },
+  {
+    "revision": "0fe1fffe32a4fadb7163",
+    "url": "/portfolio_mobile_pattern/js/app.2aaea6bd.js"
+  },
+  {
+    "revision": "51a7abebf2892feb30ec",
+    "url": "/portfolio_mobile_pattern/js/chunk-07bfe84c.51b2757d.js"
+  },
+  {
+    "revision": "f919fe2547b049d0a797",
+    "url": "/portfolio_mobile_pattern/js/chunk-0be5c82e.d16c28a1.js"
+  },
+  {
+    "revision": "5d690eedcb4830882599",
+    "url": "/portfolio_mobile_pattern/js/chunk-0c247ecf.da228563.js"
+  },
+  {
+    "revision": "71592820b39261a3b776",
+    "url": "/portfolio_mobile_pattern/js/chunk-0cb0f95c.4ac9604a.js"
+  },
+  {
+    "revision": "b03a9f55d74403074ab8",
+    "url": "/portfolio_mobile_pattern/js/chunk-0f7b1216.32166f03.js"
+  },
+  {
+    "revision": "0ade9266c6802b3b2389",
+    "url": "/portfolio_mobile_pattern/js/chunk-11936de2.bb0ae61b.js"
+  },
+  {
+    "revision": "7bf7c0297b2008e0eb77",
+    "url": "/portfolio_mobile_pattern/js/chunk-19f763a4.3f5ad616.js"
+  },
+  {
+    "revision": "6d7a50586937d30b8284",
+    "url": "/portfolio_mobile_pattern/js/chunk-1b61b144.27b0be70.js"
+  },
+  {
+    "revision": "57c13f6a05600c0704f3",
+    "url": "/portfolio_mobile_pattern/js/chunk-1d4eb874.3cf869ee.js"
+  },
+  {
+    "revision": "fccff58bb1dd9fc356f7",
+    "url": "/portfolio_mobile_pattern/js/chunk-1d6b9686.fb3a938d.js"
+  },
+  {
+    "revision": "b50b4c08822e0ecda3ea",
+    "url": "/portfolio_mobile_pattern/js/chunk-2090f038.3f3114b0.js"
+  },
+  {
+    "revision": "1b1faaaebeecdcf2f045",
+    "url": "/portfolio_mobile_pattern/js/chunk-227cb02f.43eec3fb.js"
+  },
+  {
+    "revision": "4c9371ba5c95fd1275ba",
+    "url": "/portfolio_mobile_pattern/js/chunk-2b09266e.fcdc390d.js"
+  },
+  {
+    "revision": "669fcdfeb54794269fc8",
+    "url": "/portfolio_mobile_pattern/js/chunk-2c5b5e18.90ca4a4b.js"
+  },
+  {
+    "revision": "dd00b81311684e5093b1",
+    "url": "/portfolio_mobile_pattern/js/chunk-2d0be308.9203d8f6.js"
+  },
+  {
+    "revision": "94c7650e881673eb85cb",
+    "url": "/portfolio_mobile_pattern/js/chunk-2d0e5e97.652c3d38.js"
+  },
+  {
+    "revision": "36c51d691f86c38bf9bc",
+    "url": "/portfolio_mobile_pattern/js/chunk-2d20fe69.46a32bc8.js"
+  },
+  {
+    "revision": "6ce9ddb81dd99cd5ba96",
+    "url": "/portfolio_mobile_pattern/js/chunk-2d2262db.ed4f4fc5.js"
+  },
+  {
+    "revision": "7415074ea375c5548723",
+    "url": "/portfolio_mobile_pattern/js/chunk-2d23028a.b7d57401.js"
+  },
+  {
+    "revision": "0330c7214e4fa6b255fc",
+    "url": "/portfolio_mobile_pattern/js/chunk-2e5f61a5.2ab005f9.js"
+  },
+  {
+    "revision": "14802a9d8e6eab944dac",
+    "url": "/portfolio_mobile_pattern/js/chunk-2e978e37.ec937b86.js"
+  },
+  {
+    "revision": "3c25afd72752d717a481",
+    "url": "/portfolio_mobile_pattern/js/chunk-38b64240.6d822e4b.js"
+  },
+  {
+    "revision": "a32b3feccc4b6d6e0826",
+    "url": "/portfolio_mobile_pattern/js/chunk-46fa2373.7d9c5f79.js"
+  },
+  {
+    "revision": "617833d6b4c50ce7ac55",
+    "url": "/portfolio_mobile_pattern/js/chunk-4790fb3f.f0553bb8.js"
+  },
+  {
+    "revision": "d48826e0f96d71a8d6e9",
+    "url": "/portfolio_mobile_pattern/js/chunk-483e105e.d831b4b6.js"
+  },
+  {
+    "revision": "3d1a7464f918b8a03c2c",
+    "url": "/portfolio_mobile_pattern/js/chunk-48d4bee6.a448d2fd.js"
+  },
+  {
+    "revision": "8991c894f5383676954b",
+    "url": "/portfolio_mobile_pattern/js/chunk-5510352e.c938e000.js"
+  },
+  {
+    "revision": "93543f6750a8f9650444",
+    "url": "/portfolio_mobile_pattern/js/chunk-5c758f93.bda7410f.js"
+  },
+  {
+    "revision": "e7170a3345e74d0aa7f9",
+    "url": "/portfolio_mobile_pattern/js/chunk-605577a4.e015e231.js"
+  },
+  {
+    "revision": "cbc3969e27727e1081b8",
+    "url": "/portfolio_mobile_pattern/js/chunk-6385379c.f572a7ea.js"
+  },
+  {
+    "revision": "4b921fe7ff55f68f6f49",
+    "url": "/portfolio_mobile_pattern/js/chunk-695745a4.985da54f.js"
+  },
+  {
+    "revision": "02e41295d7c063a89483",
+    "url": "/portfolio_mobile_pattern/js/chunk-6b57e0f0.0db727d7.js"
+  },
+  {
+    "revision": "dfc0b89dd7f2285b30fb",
+    "url": "/portfolio_mobile_pattern/js/chunk-6bf98b4e.e0ac2ba5.js"
+  },
+  {
+    "revision": "f93ec1d1e9ad764f9b7d",
+    "url": "/portfolio_mobile_pattern/js/chunk-70cf529a.97d9a1dd.js"
+  },
+  {
+    "revision": "68068f071d41f52ce556",
+    "url": "/portfolio_mobile_pattern/js/chunk-77d94120.d39e58d9.js"
+  },
+  {
+    "revision": "954d61a252e3b54539c0",
+    "url": "/portfolio_mobile_pattern/js/chunk-8337512c.0457fe71.js"
+  },
+  {
+    "revision": "c5822240907b077056fb",
+    "url": "/portfolio_mobile_pattern/js/chunk-87753a5a.badd372b.js"
+  },
+  {
+    "revision": "120beb6c1fc2181fb4b9",
+    "url": "/portfolio_mobile_pattern/js/chunk-8ff88cf6.c8314cbe.js"
+  },
+  {
+    "revision": "3bcc4310684bbba2af1a",
+    "url": "/portfolio_mobile_pattern/js/chunk-9cd9bf06.a77c39ff.js"
+  },
+  {
+    "revision": "50f4c84d30bae46c8530",
+    "url": "/portfolio_mobile_pattern/js/chunk-a309b180.52848dd0.js"
+  },
+  {
+    "revision": "45d81d0c952fffb37fa9",
+    "url": "/portfolio_mobile_pattern/js/chunk-ce463998.c5978247.js"
+  },
+  {
+    "revision": "212f4c05f2bb4c985beb",
+    "url": "/portfolio_mobile_pattern/js/chunk-cf54d8f8.e01edbb1.js"
+  },
+  {
+    "revision": "723376a59093dee7aef5",
+    "url": "/portfolio_mobile_pattern/js/chunk-e2efb400.d87a1548.js"
+  },
+  {
+    "revision": "83c17b20db3fdfcf7a68",
+    "url": "/portfolio_mobile_pattern/js/chunk-f3f2185e.038b9a1c.js"
+  },
+  {
+    "revision": "5fffa1e1da008954e105",
+    "url": "/portfolio_mobile_pattern/js/chunk-vendors.248016a9.js"
+  },
+  {
+    "revision": "ff3f4520ee1c52857a5816c5c41a499d",
+    "url": "/portfolio_mobile_pattern/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio_mobile_pattern/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,32 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/portfolio_mobile_pattern/precache-manifest.9468e9ee2b6e71af3175e85d0e2211b0.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "portfolio-branding"});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
